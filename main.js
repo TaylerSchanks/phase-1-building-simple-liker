a32d0de928316aca5abadfc874352562c4f3dcce
Tayler Schanks <taylerschanks2@gmail.com>
2023-04-17 14:58:06 -0500

    lab objective

--- main.js
+++ main.js
@@ -3,9 +3,67 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const heartClasses = document.querySelectorAll(".like-glyph");
+
+function fullHeart(element) {
+  element.innerText = FULL_HEART
+  element.classList.add("activated-heart")
+}
+
+function timeout() {
+ const modal =  document.getElementById("modal")
+  modal.classList.add("hidden")
+}
+
+function emptyHeart(element) {
+  element.innerText = EMPTY_HEART
+  element.classList.remove("activated-heart")
+  const modal = document.getElementById("modal")
+  modal.classList.remove("hidden")
+  setTimeout(timeout, 3000)
+}
+async function heartInteraction(e) {
+  let res = await mimicServerCall()
+  fullHeart(e.target)
+}
+
+heartClasses.forEach(h =>
+    h.addEventListener("click", heartInteraction)
+);
 
 
 
+
+
+
+// const heartClasses = document.querySelectorAll(".like-glyph");
+//
+// function fullHeart(element) {
+//   element.innerText = FULL_HEART
+//   element.classList.add("activated-heart")
+// }
+//
+// function timeout() {
+//   const modal =  document.getElementById("modal")
+//   modal.classList.add("hidden")
+// }
+//
+// function emptyHeart(element) {
+//   element.innerText = EMPTY_HEART
+//   element.classList.remove("activated-heart")
+//   const modal = document.getElementById("modal")
+//   modal.classList.remove("hidden")
+//   setTimeout(timeout, 3000)
+// }
+// function heartInteraction(e) {
+//   mimicServerCall()
+//       .then(res => fullHeart(e.target))
+//       .catch(error => emptyHeart(e.target))
+// }
+//
+// heartClasses.forEach(h =>
+//     h.addEventListener("click", heartInteraction)
+// );
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
